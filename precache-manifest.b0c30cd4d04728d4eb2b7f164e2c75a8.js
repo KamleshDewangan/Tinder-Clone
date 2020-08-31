44547ee910fd10f29767d461b2cf32bffda614f0
KamleshDewangan <kamleshd1295@gmail.com>
2020-08-31 15:30:30 +0530

    Updates

---
+++ precache-manifest.b0c30cd4d04728d4eb2b7f164e2c75a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "633a6c016c4415304dfdadbf1231e167",
+    "url": "/Tinder-Clone/index.html"
+  },
+  {
+    "revision": "7407d16af16926cbb152",
+    "url": "/Tinder-Clone/static/css/main.b8135c81.chunk.css"
+  },
+  {
+    "revision": "30cb3d91170e72769b09",
+    "url": "/Tinder-Clone/static/js/2.f8a6be81.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Tinder-Clone/static/js/2.f8a6be81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7407d16af16926cbb152",
+    "url": "/Tinder-Clone/static/js/main.c89137af.chunk.js"
+  },
+  {
+    "revision": "4cf932da34802bd4e584",
+    "url": "/Tinder-Clone/static/js/runtime-main.ae428ad0.js"
+  }
+]);
